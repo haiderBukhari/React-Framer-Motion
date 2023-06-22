b89100d20f138dbf50cd1526f204eb6d88455fa1
haiderBukhari <apshaiderbukhari786@gmail.com>
2023-06-22 23:39:15 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { motion } from 'framer-motion';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <motion.div className="app">
+        <motion.p initial = {{x: "0", y: "50%", opacity:0, scale:0}} animate = {{x: 0, y: 0, opacity:1, scale:1}} transition = {{duration: 0.3, ease: "easeOut"}}   className='hello'>Heelllo</motion.p>
+        <motion.p initial = {{x: "0", y: "-50%", opacity:0, scale:0}} animate = {{x: 0, y: 0, opacity:1, scale:1}} transition = {{duration: 0.8, ease: "easeOut"}}  >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa illo sequi qui veniam adipisci, ipsum dicta numquam praesentium doloribus quasi voluptatem molestiae sit velit id, provident fugit accusantium facere architecto!</motion.p>
+        <button initial={{x: "50%", y: "-50%", opacity:0, scale:0}} animate = {{x: 0, y: 0, opacity:1, scale:1}} transition = {{duration: 0.8, ease: "easeOut"}}  >Click Here</button>
+    </motion.div>
   );
 }
 
